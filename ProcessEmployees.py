d675ddd73a04c2b36ad846e3f4d33f0c44ccc080
Brendon <brendon_busker1@baylor.edu>
2023-02-13 13:31:01 -0600

    Finished all work

--- ProcessEmployees.py
+++ ProcessEmployees.py
@@ -11,28 +11,42 @@ Iternate through the dictionary to print out their name and thier new salary (as
 import csv
 
 #open the file
+infile = open("employee_data.csv", "r")
+reader = csv.reader(infile)
 
 
+#create an empty dictionary and list
+employee_dict = {}
+employee_list = []
 
+#use a loop to iterate through the csv file and append to list
+for row in reader:
+    employee_list.append(row)
 
-#create an empty dictionary
-
-
-#use a loop to iterate through the csv file
-
-
+#loop through list to add specific keys and values to dictionary
+for i in range(0, len(employee_list)):
     #check if the employee fits the search criteria
+    if employee_list[i][3] == "Marketing" and employee_list[i][4] == "CSR":
+        employee_dict[f"{employee_list[i][1] + ' ' + employee_list[i][2]}"] = employee_list[i][5]
 
+#prints out the dictionary before salary increase
+print()    
 
-    
+for k, v in employee_dict.items():
+    print(f"Manager Name: {k} Current Salary: ${round(float(v), 2):,}")
 
 print()
 print('=========================================')
 print()
 
-#iternate through the dictionary and print out the key and value as per printout
+#prints out the dictionary post salary increase
+for k, v in employee_dict.items():
+    print(f"Manager Name: {k} Current Salary: ${round(float(v) * 1.10, 2):,}")
 
+print()
 
+#close file
+infile.close()
 
           
         
